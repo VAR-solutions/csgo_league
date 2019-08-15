a7f395e452a1ad4516f0abe20c29a0c2f165e62a
antew7 <ananya.tewari.7@gmail.com>
2019-08-15 23:27:07 +0530

    added fixture and UI improv

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,18 @@
 <template>
   <v-container fluid pa-0 ma-0>
-    <v-toolbar color="cyan" dark flat>
-      <v-toolbar-title class="black--text">CS:GO League</v-toolbar-title>
+    <v-toolbar color="#282828" dark flat>
+      <v-toolbar-title class="t press">CS:GO League</v-toolbar-title>
 
       <v-spacer></v-spacer>
       <template v-slot:extension>
         <v-tabs
-          color="black"
+          color="#91ff00"
           v-model="tab"
           active-class
           align-with-title
           background-color="transparent"
         >
-          <v-tabs-slider color="black"></v-tabs-slider>
+          <v-tabs-slider color="#91ff00"></v-tabs-slider>
 
           <v-tab v-for="(item,i ) in items" :key="i" :to="{ name : link[i]}">{{ item }}</v-tab>
         </v-tabs>
@@ -22,6 +22,13 @@
   </v-container>
 </template>
 
+
+<style>
+.t{
+  color:#91ff00;
+
+}
+</style>
 <script>
 
 export default {
--- src/views/Fixtures.vue
+++ src/views/Fixtures.vue
@@ -1,13 +1,52 @@
 <template>
     <v-container>
-        <h2>Fixtures</h2>
-        <h2>Yet to be announced</h2>
+        <v-timeline align-top :dense="$vuetify.breakpoint.smAndDown">
+    <v-timeline-item
+      v-for="(item, i) in fixtures"
+      :key="i"
+      
+      color="#91ff00"
+      
+      fill-dot
+    >
+      <v-card
+      
+        dark
+      >
+        <v-card-title class="title fin">Match #{{ item.mnumber }}</v-card-title>
+        <v-card-text class="lexend">
+          <p>Date: {{ item.date }}</p>
+          <p style="font-size: 20px">{{item.team1}} vs {{item.team2}}</p>
+          
+        </v-card-text>
+      </v-card>
+    </v-timeline-item>
+  </v-timeline>
     </v-container>
 </template>
 
 <script>
+import firebase from "firebase/app";
+import "firebase/firestore";
+import db from "../firebase";
 export default {
     name: "fixtures",
+    created() {
+    db.firestore()
+      .collection("fixtures").orderBy("mnumber")
+      .get()
+      .then(res => {
+        res.forEach(doc => {
+          this.fixtures.push(doc.data());
+          console.log(doc.data())
+        });
+      });
+  },
+    data(){
+     return {
+        fixtures: []
+    }
+    }
     
 }
 </script>
