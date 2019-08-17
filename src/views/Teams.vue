6d7f4595f86880492861353cba6e23ea81e8e5e2
antew7 <ananya.tewari.7@gmail.com>
2019-08-17 11:35:03 +0530

    Infinite Fixes

--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -1,12 +1,37 @@
 <template>
-    <v-container>
-        <h2>Team</h2>
-        <h2>Yet to be announced</h2>
-    </v-container>
+<v-container grid-list-sm align-content-space-between>
+    <v-layout wrap>
+      <Team v-for="(ex, k) in this.teams" :key="k" :data="ex" />
+    </v-layout>
+  </v-container>
+    
 </template>
 
 <script>
+import firebase from "firebase/app";
+import "firebase/firestore";
+import db from "../firebase";
+import  Team from "../components/Team";
 export default {
     name: "teams",
-}
+    components: {
+        Team
+    },
+    created() {
+    db.firestore()
+      .collection("teams")
+      .onSnapshot(res => {
+        this.teams = []
+        res.forEach(doc => {
+          this.teams.push(doc.data());
+          console.log(doc.data());
+        });
+      });
+},
+data() {
+    return {
+      teams: []
+    };
+  }
+};
 </script>
