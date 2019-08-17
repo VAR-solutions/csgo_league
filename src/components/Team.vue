9a6130016dba2a45614c9b7202582bae5189d2d3
Ravi Varshney <ravivarshney49@gmail.com>
2019-08-17 14:59:09 +0530

    added team view and minor fixes

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,11 +1,13 @@
 <template>
   <v-flex xs12 sm6 md4>
-    <v-card class="mx-auto my-2" width="95%" tile >
+    <v-card class="mx-auto my-2" width="95%" tile :to="'/teams/'+data.tid"   >
       <v-layout align-center justify-center pt-4>
-        <v-avatar size="75%">
-          <img
+        <v-avatar size="240">
+          <img v-if="!data.photo"
             src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/deadpool-1563973577.jpg?crop=0.563xw:1.00xh;0,0&resize=480:*"
           />
+          <img v-else-if="data.photo"
+            :src="data.photo" />
         </v-avatar>
       </v-layout>
       <v-layout align-center justify-center>
