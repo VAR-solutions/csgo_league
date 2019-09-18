f037f5f800d66b86bf5f9e804afc5c97907eb6e3
i-vishi <rr.vishalgaur@gmail.com>
2019-09-18 18:54:48 +0530

    updated fixtures

--- src/views/Fixtures.vue
+++ src/views/Fixtures.vue
@@ -7,12 +7,22 @@
     <Loader />
     <v-timeline align-top :dense="$vuetify.breakpoint.smAndDown">
       <v-timeline-item v-for="(item, i) in fixtures" :key="i" color="#91ff00" fill-dot>
+        <template v-slot:icon>
+          <span>
+            <v-icon v-if="item.flag">mdi-checkbox-marked-circle-outline</v-icon>
+          </span>
+        </template>
         <v-card dark>
           <v-card-title class="title fin">Match #{{ item.mnumber }}</v-card-title>
           <v-card-text class="lexend">
             <p>Date: {{ item.date }}</p>
             <p style="font-size: 20px">{{item.team1}} vs {{item.team2}}</p>
           </v-card-text>
+          <v-card-actions>
+            <p v-if="item.flag" class="title fin">{{ item.wonby }} won the match</p>
+            <!-- <div class="flex-grow-1"></div> -->
+            <!-- <v-btn v-if="item.flag" @click="dialogScore = true">See Scorecard</v-btn> -->
+          </v-card-actions>
         </v-card>
       </v-timeline-item>
     </v-timeline>
